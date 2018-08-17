89fbc48f4c749d75e0d5c1f78ea8f33131ea9d0a
yochananlove1 <42241202+yochananlove1@users.noreply.github.com>
2018-08-17 10:15:42 +0100

    Add files via upload

---
+++ Assignment 3.py
@@ -0,0 +1,9 @@
+#Program to determine a random number
+a = 75
+while ( a <= 100):
+    print (a)
+    a = a + 1    
+if (a <= 100):
+    print ('you are close, try again')
+else: (a >= 100)
+print ('you are not close, try again')
